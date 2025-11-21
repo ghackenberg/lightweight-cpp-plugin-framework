82ddeb585831c69853c0774790c86d51a19b2518
Dr. Georg Hackenberg <7525462+ghackenberg@users.noreply.github.com>
2025-11-21 22:54:28 +0100

    delete namespace files for now

--- Sources/Plugins/DummyApplication/Namespace.h
+++
@@ -1,6 +0,0 @@
-/**
- * \namespace Plugins::DummyApplication
- * \brief Sample application service plugin.
- *
- * This minimal plugin demonstrates the basics of plugin development.
- */
--- Sources/Plugins/DummyService/Namespace.h
+++
@@ -1,6 +0,0 @@
-/**
- * \namespace Plugins::DummyService
- * \brief Sample service plugin.
- *
- * This minimal plugin demonstrates the basics of plugin development.
- */
